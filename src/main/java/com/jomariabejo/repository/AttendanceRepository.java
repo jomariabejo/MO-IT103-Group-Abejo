d5260a9a8c1bfbc2424fa88ca6530a7149ef9761
jomariabejo <lr.jabejo@mmdc.mcl.edu.ph>
2023-09-23 20:02:21 +0800

    feat: attendance crud

--- src/main/java/com/jomariabejo/repository/AttendanceRepository.java
+++ src/main/java/com/jomariabejo/repository/AttendanceRepository.java
@@ -10,13 +10,13 @@ import jakarta.persistence.EntityTransaction;
 import java.util.Date;
 import java.util.List;
 
-public class AttendanceRepository implements iAttendanceRecord {
+public class AttendanceRepository {
 
     /**
-     * Retrieves a list of Attendance records for a given employee based on their employeeID.
+     * Retrieves a list of Attendance records for a given employee based on their employeeNumber.
      *
      * @param employeeNumber The unique identifier of the employee.
-     * @return A List of Attendance objects associated with the specified employeeID.
+     * @return A List of Attendance objects associated with the specified employeeNumber.
      */
     List<Attendance> getAllAttendance(long employeeNumber) {
         return db.getInstance().getEntityManager()
@@ -24,7 +24,6 @@ public class AttendanceRepository implements iAttendanceRecord {
                 .setParameter("employeeID", employeeNumber)
                 .getResultList();
     }
-
 
 
     /**
@@ -43,61 +42,103 @@ public class AttendanceRepository implements iAttendanceRecord {
     }
 
     /**
-     * Inserts new attendance row to attendance tbl
+     * Retrieves a list of Attendance records for a given employee, year, and month.
+     *
+     * @param employeeNumber The employee's unique identifier.
+     * @param year           The year for which attendance records are to be retrieved.
+     * @param month          The month for which attendance records are to be retrieved.
+     * @return A List of Attendance objects associated with the specified employeeNumber, year, and month.
+     */
+    List<Attendance> getAllAttendance(int employeeNumber, int year, int month) {
+        return db.getInstance().getEntityManager()
+                .createQuery("SELECT a FROM Attendance a WHERE a.employeeNumber = :employeeNumber AND YEAR(a.date) = :year AND MONTH(a.date) = :month", Attendance.class)
+                .setParameter("employeeNumber", employeeNumber)
+                .setParameter("year", year)
+                .setParameter("month", month)
+                .getResultList();
+    }
+
+
+    /**
+     * Inserts a new attendance record into the attendance table if it doesn't already exist.
+     *
+     * @param attendance The new attendance record to be added
      */
     public static void createAttendance(Attendance attendance) {
-        /** Declare arguments, for firstname, lastname and date */
-        String
-                fastName = attendance.getFirstName() ,
-                lastName = attendance.getLastName();
-        Date
-                date = attendance.getDate();
+        EntityManager em = db.getInstance().getEntityManager();
 
-        /** Check if attendance already exist */
-        Boolean isAttendanceExist = db.getInstance().getEntityManager().createQuery("SELECT c FROM Attendance c WHERE c.firstName = firstName AND c.lastName = lastName AND c.date = date").getSingleResult() != null;
+        String firstName = attendance.getFirstName();
+        String lastName = attendance.getLastName();
+        Date date = attendance.getDate();
 
-        /** If it don't exist then... Insert new attendance...*/
-        if (isAttendanceExist) {
-            db.getInstance().getEntityManager().persist(attendance);
+        Long attendanceCount = em.createQuery("SELECT COUNT(c) FROM Attendance c WHERE c.firstName = :firstName AND c.lastName = :lastName AND c.date = :date", Long.class)
+                .setParameter("firstName", firstName)
+                .setParameter("lastName", lastName)
+                .setParameter("date", date)
+                .getSingleResult();
+
+        if (attendanceCount == 0) {
+            EntityTransaction transaction = em.getTransaction();
+
+            try {
+                transaction.begin();
+                em.persist(attendance);
+                transaction.commit();
+                System.out.println("Attendance added successfully...");
+            } catch (Exception e) {
+                if (transaction != null && transaction.isActive()) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            } finally {
+                em.close();
+            }
+        } else {
+            System.out.println("Attendance already exists...");
         }
-        else {
-            System.out.println("Attendance already exist...");
+    }
+
+    /**
+     * Removes attendance records by their unique ID.
+     * @param id The ID of the attendance record to be deleted.
+     */
+    public static void deleteAttendance(Long id) {
+        try {
+            EntityManager entityManager = db.getInstance().getEntityManager();
+            EntityTransaction entityTransaction = entityManager.getTransaction();
+            Employee employee = entityManager.find(Employee.class, id);
+            if (employee != null) {
+                entityTransaction.begin();
+                entityManager.remove(employee);
+                entityTransaction.commit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
 
-    public static void deleteAttendance(Long id) {
+    /**
+     * Updates an existing attendance record.
+     *
+     * @param attendanceID The unique ID of the attendance record to be updated.
+     * @param attendance   The updated Attendance object containing new values.
+     */
+    public static void updateAttendance(Long attendanceID, Attendance attendance) {
         EntityManager entityManager = db.getInstance().getEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
 
-        Employee employee = entityManager.find(Employee.class, id);
+        Attendance attendanceToBeUpdated = entityManager.find(Attendance.class, attendanceID);
 
-        if (employee != null) {
-            entityTransaction.begin();
-            entityManager.remove(employee);
-            entityTransaction.commit();
-        }
+        attendanceToBeUpdated.setEmployeeNumber(attendance.getEmployeeNumber());
+        attendanceToBeUpdated.setLastName(attendance.getLastName());
+        attendanceToBeUpdated.setFirstName(attendance.getFirstName());
+        attendanceToBeUpdated.setDate(attendance.getDate());
+        attendanceToBeUpdated.setTimeIn(attendance.getTimeIn());
+        attendanceToBeUpdated.setTimeOut(attendance.getTimeOut());
 
+        entityTransaction.begin();
+        entityTransaction.commit();
         entityManager.close();
-    }
-
-    public static Employee getSpecifiedAttendance(Long id) {
-        return db
-                .getInstance()
-                .getEntityManager()
-                .find(Employee.class, id);
-    }
-
-    public static int getAttendanceYearSize() {
-        return db.getInstance().getEntityManager()
-                .createQuery("SELECT c FROM Employee c", Employee.class)
-                .getResultList()
-                .size();
-    }
-
-    public static void main(String[] args) {
-        deleteAttendance(1L);
-        System.out.println(getAttendanceYearSize());
-//        System.out.println(getEmployee(1L).getEmployeeNumber());
     }
 }
--- src/main/test/attendance/createAttendanceTest.java
+++ src/main/test/attendance/createAttendanceTest.java
@@ -3,6 +3,7 @@ package attendance;
 import com.jomariabejo.model.Attendance;
 import com.jomariabejo.repository.AttendanceRepository;
 
+import java.sql.Time;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -10,19 +11,21 @@ import java.util.Date;
 
 public class createAttendanceTest {
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String strDate = "2022-01-09";
-
-        Date date = new Date(sdf.format(String.valueOf(DateFormat.getDateInstance().parse(strDate))));
-
+        String strDate = "01-09-2022";
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = df.parse(strDate);
         System.out.println(date);
+
+        Time timeIn = new Time(8,0,0);
+        Time timeOut = new Time(17, 0, 0);
+
 //        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-DD");
 //
 //        simpleDateFormat.format(strDate);
 //
 //        System.out.println(simpleDateFormat.format(""));
 //
-//        Attendance attendance = new Attendance(100001, "Crisostomo", "Jose", "2022-01-09");
-//        AttendanceRepository.createAttendance();
+        Attendance attendance = new Attendance(10001, "Crisostomo", "Jose",date, timeIn, timeOut);
+        AttendanceRepository.createAttendance(attendance);
     }
 }
