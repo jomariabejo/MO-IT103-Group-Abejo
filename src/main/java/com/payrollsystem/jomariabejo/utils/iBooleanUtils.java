8ab15e58f023f28dfc621a154e1755a8b45ade25
jomariabejo <lr.jabejo@mmdc.mcl.edu.ph>
2023-12-22 08:31:39 +0800

    refactor iBooleanUtils interface: Split isFirstCharQuoteOrWhiteSpace method into focused checks

--- src/main/java/com/payrollsystem/jomariabejo/utils/iBooleanUtils.java
+++ src/main/java/com/payrollsystem/jomariabejo/utils/iBooleanUtils.java
@@ -2,8 +2,21 @@ package com.payrollsystem.jomariabejo.utils;
 
 public interface iBooleanUtils {
 
-    // Return true if the first character is a quote or whitespace, false otherwise
-    default boolean isFirstCharQuoteOrWhiteSpace(String str) {
-        return str.charAt(0) == ' ' || str.charAt(0) == '\"';
+    /**
+     * Checks if the first character of a string is a double quote (").
+     * @param inputString The String to be tested
+     * @return True if it begins double quote, false otherwise.
+     */
+    default boolean isFirstCharDoubleQuote(String inputString) {
+        return  inputString.startsWith("\"");
+    }
+
+    /**
+     * Checks if the first character of a string is a whitespace ( ).
+     * @param inputString The string to be tested
+     * @return True if it begins whitespace, false otherwise;
+     */
+    default boolean isFirstCharWhitespace(String inputString) {
+        return inputString.startsWith(" ");
     }
 }
