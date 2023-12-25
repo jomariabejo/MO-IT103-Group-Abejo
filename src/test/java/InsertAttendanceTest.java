0ba4f896871f8228d0d73adafdf025ea8dd28701
jomariabejo <lr.jabejo@mmdc.mcl.edu.ph>
2023-12-25 22:09:26 +0800

    test: Improve code readability and add comments
    - Enhanced code readability by adding descriptive comments for InsertAttendanceTest
    - Added clarifying comments to the test method explaining each step in the process of testing attendance insertion
    - Left a TODO for future work on creating test cases for update and delete functionalities for Attendance

--- src/test/java/InsertAttendanceTest.java
+++ src/test/java/InsertAttendanceTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 public class InsertAttendanceTest {
     @Test
     public void addNewAttendanceTest() {
+        // Define a new Attendance object for Jomari Abejo on December 25, 2023, from 8:00 AM to 5:00 PM
         Attendance attendance = new Attendance(
                 10000,
                 "Abejo",
@@ -15,8 +16,12 @@ public class InsertAttendanceTest {
                 "8:00",
                 "17:00"
         );
+
+        // Create a new attendance entry using the CSVAttendanceRepository
         new CSVAttendanceRepository().createAttendance(attendance);
-        String [] verifiedAttendance = {
+
+        // Prepare verified attendance data for comparison
+        String[] verifiedAttendance = {
                 "10000",
                 "Abejo",
                 "Jomari",
@@ -24,6 +29,13 @@ public class InsertAttendanceTest {
                 "8:00",
                 "17:00"
         };
-        assertTrue(new CSVAttendanceRepository().hasMatchingAttendance(attendance,verifiedAttendance));
+
+        // Check if the created attendance matches the verified attendance details
+        assertTrue(new CSVAttendanceRepository().hasMatchingAttendance(attendance, verifiedAttendance));
+
+        // Check if the verified attendance exists in the CSVAttendanceRepository
+        assertTrue(new CSVAttendanceRepository().isAttendanceExist(verifiedAttendance));
+
+        // TODO: 12/26/2023 - Create test cases for update and delete functionalities for Attendance.
     }
 }
